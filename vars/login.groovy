41cc3f7b74be75814d57d77ec3d1cd64d851684d
Sanjai Ragul M <sanjairagulmurugesan@presidio.com>
2024-12-16 11:55:09 +0530

    Iniial commit v0.1

--- vars/login.groovy
+++ vars/login.groovy
@@ -1,3 +1,11 @@
 def call(Map config = [:]) {
-    sh "aws ecr get-login-password --region ${config.region} | docker login --username AWS --password-stdin ${config.repo_uri}"
+    // Export AWS credentials as environment variables
+    withEnv([
+        "AWS_ACCESS_KEY_ID=${config.awsAccessKeyId}",
+        "AWS_SECRET_ACCESS_KEY=${config.awsSecretAccessKey}",
+        "AWS_SESSION_TOKEN=${config.awsSessionToken}"
+    ]) {
+        // Execute the login command using the provided AWS credentials
+        sh "aws ecr get-login-password --region ${config.region} | docker login --username AWS --password-stdin ${config.repo_uri}"
+    }
 }
